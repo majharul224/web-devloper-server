4db5547b00ffaac28694bcc0a255a543ae0856ef
majharul224 <majharulislam2225@gmail.com>
2022-10-27 14:57:29 +0600

    cors setup serversite

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 
+const course = require('./data/course.json')
 app.get('/', (req, res) =>{
     res.send("course api runing")
 });
 
+app.get('/course-categories',(req, res) =>{
+    res.send(course)
+})
 
 app.listen(port, ()=>{
     console.log("server corse loder")
