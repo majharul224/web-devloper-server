3fb56a5b7326cd2b7b70cbb98786865ae412a47e
majharul224 <majharulislam2225@gmail.com>
2022-10-27 21:43:03 +0600

    course id api lode

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const course = require('./data/course.json')
+
 app.get('/', (req, res) =>{
     res.send("course api runing")
 });
@@ -13,6 +14,13 @@ app.get('/', (req, res) =>{
 app.get('/course-categories',(req, res) =>{
     res.send(course)
 })
+
+app.get('/course-categories/:id', (req, res)=>{
+    const id= parseInt(req.params.id); 
+  const cour = course.find(c => c.id === id ) || {} 
+  res.send(cour) 
+})
+
 
 app.listen(port, ()=>{
     console.log("server corse loder")
